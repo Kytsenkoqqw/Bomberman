d5f70ab08b75c1a75441791c275da13aaca31edc
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-12-24 18:45:40 +0200

    Add Navmesh

--- Bomberman/Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Bomberman/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -8,41 +8,17 @@ using Random = UnityEngine.Random;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed = 3f;
-    private Vector2 _startDirection;
-    
+    [SerializeField] private Transform _target;
+
+    private NavMeshAgent agent;
+
     private void Start()
     {
-        // Задаём начальное случайное направление
-        Vector2[] directions = { Vector2.down, Vector2.left, Vector2.right, Vector2.up };
-        _startDirection = directions[Random.Range(0, directions.Length)];
+        agent.GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        // Двигаем объект в выбранном направлении
-        transform.Translate(_startDirection * _moveSpeed * Time.deltaTime);
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        // Проверка на столкновение с объектами на слоях "obstacles" или "interactable"
-        if (other.gameObject.CompareTag("interactable") || other.gameObject.CompareTag("Obstacles"))
-        {
-            Debug.Log("Enemy bam");
-
-            // Задаём новое случайное направление, исключая текущее
-            Vector2[] directions = { Vector2.down, Vector2.left, Vector2.right, Vector2.up };
-            Vector2 newDirection;
-
-            // Повторяем выбор, пока не получим направление, отличное от текущего
-            do
-            {
-                newDirection = directions[Random.Range(0, directions.Length)];
-            } while (newDirection == _startDirection); // Проверка, что новое направление не совпадает с текущим
-
-            // Обновляем направление
-            _startDirection = newDirection;
-        }
+        agent.SetDestination(_target.position);
     }
 }
