4102255956b368063769c33da3b98ceab588d7da
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2025-01-20 18:17:16 +0200

    NavMesh setting

--- Bomberman/Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Bomberman/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -8,40 +8,13 @@ using Random = UnityEngine.Random;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    private Vector2 _startDirection;
-    [SerializeField] private float _moveSpeed = 3f;
-    [SerializeField] private float _gridSize = 1f;
-    
-
-    private void Start()
-    {
-        Vector2[] directions = {Vector2.down, Vector2.up, Vector2.left, Vector2.right,};
-        _startDirection = directions[Random.Range(0, directions.Length)];
-        transform.position = SnapToGrid(transform.position);
-        Debug.Log(_startDirection);
-    }
+    [SerializeField] private float _moveSpeed = 1.5f;
+    [SerializeField] private Transform _target;
 
     private void Update()
     {
-        transform.Translate(_startDirection * _moveSpeed * Time.deltaTime);
-    }
-    
-    private Vector2 SnapToGrid(Vector2 position)
-    {
-        // Привязываем к центру ячейки
-        float snappedX = Mathf.Floor(position.x / _gridSize) * _gridSize + _gridSize / 2;
-        float snappedY = Mathf.Floor(position.y / _gridSize) * _gridSize + _gridSize / 2;
-
-        return new Vector2(snappedX, snappedY);
+        transform.position = Vector2.MoveTowards(transform.position, _target.position, _moveSpeed * Time.deltaTime);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Obstacles"))
-        {
-            Debug.Log("obstacles");
-            Vector2[] directions = {Vector2.down, Vector2.up, Vector2.left, Vector2.right,};
-            _startDirection = directions[Random.Range(0, directions.Length)];
-        }
-    }
+
 }
