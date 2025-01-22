7e16bbc4cd031ff60460a46b31e8e9653e04b79f
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2025-01-22 17:42:42 +0200

    Rewor enemy ai

--- Bomberman/Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Bomberman/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -8,28 +8,43 @@ using Random = UnityEngine.Random;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    [SerializeField] private float _wanderRadius = 10f; // Радиус, в котором будут генерироваться точки
+    [SerializeField] private float _waitTime = 2f;      // Время ожидания перед движением к новой точке
     private NavMeshAgent _agent;
-    [SerializeField] private Transform _target;
-    
+    private float _timer;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _agent.updateRotation = false;
+        MoveToRandomPoint();
         _agent.updateUpAxis = false;
+        _agent.updateRotation = false;
     }
 
     private void Update()
     {
-        if (_target == null )
+        // Проверяем, достиг ли агент текущей цели
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
         {
-            Debug.Log("Target is null");
+            _timer += Time.deltaTime;
+            if (_timer >= _waitTime)
+            {
+                MoveToRandomPoint();
+                _timer = 0f;
+            }
         }
-        else
+    }
+
+    private void MoveToRandomPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * _wanderRadius; // Генерация случайного направления
+        randomDirection += transform.position; // Смещение в локальные координаты
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomDirection, out hit, _wanderRadius, NavMesh.AllAreas))
         {
-            _agent.SetDestination(_target.position);
+            hit.position = new Vector2(Mathf.Floor(hit.position.x) + 0.5f, Mathf.Floor(hit.position.y) + 0.5f);
+            _agent.SetDestination(hit.position); // Устанавлив
         }
-        
-        
     }
 }
